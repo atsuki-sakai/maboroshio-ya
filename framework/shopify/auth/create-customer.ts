a57d299199234b12e2d696abe0802b092b56b555
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2022-11-04 04:26:32 +0900

    refacter

--- framework/shopify/auth/create-customer.ts
+++ framework/shopify/auth/create-customer.ts
@@ -2,7 +2,7 @@
 import { CustomerCreatePayload } from "@shopify/shema"
 import { generateAdminApiPath } from "@shopify/utils/generate-admin-api-path"
 
-const createCustomer = async (email: string): Promise<CustomerCreatePayload> => {
+const createCustomer = async (email: string): Promise<any> => {
     console.log("create customer api @shopfy/auth ",)
     const createCustomerApiUrl = generateAdminApiPath({type:"CREATE_CUSTOMER"})!
     const  response = await fetch(createCustomerApiUrl, {
