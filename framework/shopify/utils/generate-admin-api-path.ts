135890f0aa27d74f503fbd5aafb3a73e40fd0374
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2022-11-04 03:27:31 +0900

    refacter

--- framework/shopify/utils/generate-admin-api-path.ts
+++ framework/shopify/utils/generate-admin-api-path.ts
@@ -3,7 +3,7 @@ export type ApiPathType = {
     type: "CREATE_CUSTOMER"
 }
 
-const hostingUrl = process.env.NEXT_PUBLIC_HOSTING_DOMAIN_URL as string
+const hostingUrl = process.env.NEXT_PUBLIC_HOSTING_DOMAIN as string
 
 export const generateAdminApiPath = (apitype: ApiPathType) => {
     switch(apitype.type){
