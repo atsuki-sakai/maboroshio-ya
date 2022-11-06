29a61238f63379feb722c768d57a3fee4e5345c2
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2022-11-06 11:36:51 +0900

    refacter

--- components/ui/ProductView/ProductView.tsx
+++ components/ui/ProductView/ProductView.tsx
@@ -27,10 +27,8 @@ const ProductView: FC<Props> = ({ product }) => {
         const lastName = "atsuki"
         const phone = "07090308805"
         const { customer, customerUserErrors } = await createCustomer(email, password, acceptsMarketing, firstName, lastName, phone);
-        if(customerUserErrors[0]){
-            console.log(customerUserErrors[0].message)
-        }
-        console.log(customer && customer?.email)
+        console.log(customer)
+        console.log(customerUserErrors)
     }
     return (
         <>
