c889fc1515e446db0d792f61d1e601adde7850be
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2022-11-08 11:38:49 +0900

    refacter

--- framework/shopify/utils/getCheckout.ts
+++ framework/shopify/utils/getCheckout.ts
@@ -11,10 +11,10 @@ const getCheckout = async(id: string) => {
             id: id
         })
     });
-    
     const data = await response.json();
 
-    console.log(data.node)
+    console.log(data)
+   
 
     return data.node;
 }
