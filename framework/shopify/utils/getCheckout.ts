b47a736d395438474f047e19c85b70f5e753b0d9
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2022-11-08 11:44:53 +0900

    refacter

--- framework/shopify/utils/getCheckout.ts
+++ framework/shopify/utils/getCheckout.ts
@@ -1,6 +1,7 @@
+import { Checkout } from "@shopify/shema";
 import { generateApiUrl } from "./generate-api-url";
 
-const getCheckout = async(id: string) => {
+const getCheckout = async(id: string): Promise<Checkout> => {
 
     const getCheckoutApiUrl = generateApiUrl({type: "GET_CHECKOUT"})
 
@@ -11,14 +12,10 @@ const getCheckout = async(id: string) => {
             id: id
         })
     });
-    const data = await response.json();
+    const { data } = await response.json();
+    const checkout = data.node as Checkout
 
-    console.log(data)
-    console.log(data.data)
-    console.log(data.data.node)
-   
-
-    return data.node;
+    return checkout;
 }
 
 export default getCheckout
