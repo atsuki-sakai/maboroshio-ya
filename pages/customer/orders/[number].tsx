f509fe5dcb21ade3bfc8201968debebe34b8f7fd
酒井専冴 <atsukisakai@sakaisensaenoMacBook-Pro.local>
2022-12-09 09:49:00 +0900

    refacter

--- pages/customer/orders/[number].tsx
+++ pages/customer/orders/[number].tsx
@@ -12,10 +12,25 @@ import { GetStaticProps } from 'next';
 const OrderDetail = () => {
 
     const router = useRouter()
-    console.log(router.asPath)
+    const { loggedCustomer } = useCustomerState()
+    const orderNumber = router.asPath.split('orders/')[1]
+    let choiceOrder
+    useEffect(() => {
+        if(orderNumber){
+            loggedCustomer?.orders.edges.map(({node: order}) => {
+                choiceOrder = order.orderNumber.toString() === orderNumber.toString()
+            })
+        }
+    },[orderNumber])
+
+
+
     return (
         <Container>
             <div className='px-8'>
+                {
+                    JSON.stringify(choiceOrder, null, 2)
+                }
                 {/* <p className='text-xs text-gray-500 mb-1'>注文番号 <span className='font-bold'>{order.orderNumber}</span></p>
                 <p className='text-xs text-gray-500 mb-1'>注文日 <span className='font-bold'>{order.processedAt}</span></p> */}
             </div>
